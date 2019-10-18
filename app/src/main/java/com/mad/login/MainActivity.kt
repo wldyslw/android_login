c42113b83364ab69606da7f3794ec1ca5e8ddac0
Uladzislau Maltsau <umaltsau@dealcloud.com>
2019-10-18 14:55:40 +0300

    Redundant imports removed

--- app/src/main/java/com/mad/login/MainActivity.kt
+++ app/src/main/java/com/mad/login/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.mad.login.databinding.ActivityMainBinding
 
--- app/src/main/java/com/mad/login/WelcomeActivity.kt
+++ app/src/main/java/com/mad/login/WelcomeActivity.kt
@@ -1,10 +1,7 @@
 package com.mad.login
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.view.View
 
 class WelcomeActivity : AppCompatActivity() {
 
